73732568babce14b0d6af301e4b16ecccfbf502e
suYan <709936456@qq.com>
2025-06-28 12:56:24 +0800

    fix-部门前端发送查询对象，后端接收数据格式需要一致

---
+++ src/main/java/com/admin/module/system/query/DeptQuery.java
@@ -0,0 +1,10 @@
+package com.admin.module.system.query;
+
+import com.admin.common.base.BasePage;
+import lombok.Data;
+
+@Data
+public class DeptQuery extends BasePage {
+
+    private String keyWord;
+}
