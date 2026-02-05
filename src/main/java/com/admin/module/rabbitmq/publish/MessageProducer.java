b246e4eaf613d026a73e6e0db26d17c5a976c7d8
Syxi <709936456@qq.com>
2026-02-05 17:33:36 +0800

    fix-删除rabbitmq代码

--- src/main/java/com/admin/module/rabbitmq/publish/MessageProducer.java
+++ src/main/java/com/admin/module/rabbitmq/publish/MessageProducer.java
@@ -1,21 +1,16 @@
-package com.admin.module.rabbitmq.publish;
-
-import com.admin.module.rabbitmq.config.RabbitmqConfig;
-import jakarta.annotation.Resource;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.stereotype.Component;
+//package com.admin.module.rabbitmq.publish;
 
 /**
  * 测试类，发送mq消息
  */
-@Component
-public class MessageProducer {
-
-    @Resource
-    private RabbitTemplate rabbitTemplate;
-
-
-    public void sendMessage(String message) {
-        rabbitTemplate.convertAndSend(RabbitmqConfig.EXCHANGE_NAME, RabbitmqConfig.ROUTING_KEY, message);
-    }
-}
+//@Component
+//public class MessageProducer {
+//
+//    @Resource
+//    private RabbitTemplate rabbitTemplate;
+//
+//
+//    public void sendMessage(String message) {
+//        rabbitTemplate.convertAndSend(RabbitmqConfig.EXCHANGE_NAME, RabbitmqConfig.ROUTING_KEY, message);
+//    }
+//}
